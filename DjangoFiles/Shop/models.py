cbcc571698ae974eb512a522444c7f6f055f44e9
Gromov Artem <gromov@pop-os.localdomain>
2020-12-08 19:07:09 +0300

    Create to SQL tables

---
+++ DjangoFiles/Shop/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+    """
+    Таблица, содержащая товар: 
+    - id товара;
+    - Количество
+    - цена
+    - Название товара
+    - Описание 
+    """
+    id = models.IntegerField(default=0)
+    amount = models.IntegerField(default=0)
+    price = models.FloatField(default=0)
+    name = models.CharField(max_length=200)
+
+class Users(models.Model):
+    """
+    Таблица, содержащая данные о пользователях:
+    - Адрес кошелька;
+    - Логин;
+    - Пароль;
+    - Роль пользователя на сайте(пока сделаю так: 0 - админ, 1 - клиент, 2 - Курьер);
+    """
+    wallet_adress = models.CharField(max_length=200)
+    login = models.EmailField()
+    password = models.CharField(max_length=25)
+    role = models.IntegerField(default=0)

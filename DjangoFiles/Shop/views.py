fed7a0a7cfc214897e308fdd3c084919e47f1e65
Gromov Artem <gromov@pop-os.localdomain>
2020-12-08 22:52:10 +0300

    index page loading well

--- DjangoFiles/Shop/views.py
+++ DjangoFiles/Shop/views.py
@@ -1,3 +1,16 @@
+import django
+from django.http.response import HttpResponse
 from django.shortcuts import render
-
+from django.views import View
+from django.http import HttpResponse
 # Create your views here.
+
+class IndexView(View):
+    """
+    docstring
+    """
+    def get(self, request):
+        """ 
+        docstring
+        """
+        return render(request, 'Shop/index.html')

ab7f88ea4c16bd093b696036cb1564ec99148d38
IamAdite <67636488+IamAdite@users.noreply.github.com>
2020-12-10 18:18:30 +0500

    Update Escrow_Test.sol

--- Escrow_Test.sol
+++ Escrow_Test.sol
@@ -52,7 +52,7 @@ contract EscrowBaseContract {
         else return 'Refunded.';
     }
     
-    function _getState(uint id) internal view returns(State currentState) {
+    function _getState(uint id) external view returns(State currentState) {
         return order_list[id].currentState;
     }
  

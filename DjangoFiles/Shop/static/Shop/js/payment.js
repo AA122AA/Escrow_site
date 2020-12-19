80d283a6768b27fcfb9ba6148ca4360c023a8c5b
AA122AA <gromartem2000@gmail.com>
2020-12-19 22:03:49 +0300

    replace main

---
+++ DjangoFiles/Shop/static/Shop/js/payment.js
@@ -0,0 +1,99 @@
+import {connect} from "/login.js"
+
+function initContract() {
+    const abi = [{
+        "constant": true,
+        "inputs": [{"name": "id", "type": "uint256"}],
+        "name": "CheckState",
+        "outputs": [{"name": "currentState", "type": "uint8"}],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    }, {
+        "constant": false,
+        "inputs": [{"name": "id", "type": "uint256"}],
+        "name": "Refund",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    }, {
+        "constant": false,
+        "inputs": [{"name": "id", "type": "uint256"}],
+        "name": "delivered",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    }, {
+        "constant": true,
+        "inputs": [{"name": "", "type": "uint256"}],
+        "name": "order_list",
+        "outputs": [{"name": "buyer", "type": "address"}, {
+            "name": "deposit",
+            "type": "uint256"
+        }, {"name": "currentState", "type": "uint8"}, {"name": "OrderTime", "type": "uint32"}],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    }, {
+        "constant": false,
+        "inputs": [],
+        "name": "deposit",
+        "outputs": [],
+        "payable": true,
+        "stateMutability": "payable",
+        "type": "function"
+    }];
+    const contract_Address = {
+        "3": "0x4b41F4A10BB68Df8890B789273666bA2EDe0c2F8"
+    }
+    try {
+        var current_network = web3.version.network;
+        var contract = web3.eth.contract(abi).at(contract_Address[current_network]);
+    } catch (error) {
+        console.log(error)
+    }
+    return contract;
+}
+
+function deposit_js(price) {
+    connect();
+    console.log(price)
+    var contract = initContract();
+    const amountWei = price / 45000 * 1e18
+    contract.deposit({
+        value: amountWei
+    }, function (error, result) {
+        if (!error)
+            console.log(result);
+        else
+            console.error(error);
+    });
+}
+
+function change_product(addr) {
+    console.log("create post is working!") // sanity check
+    $.ajax({
+        type: "POST", // http method
+        data: {
+            the_post: addr,
+            csrfmiddlewaretoken: window.CSRF_TOKEN
+        }, // data sent with the post request
+
+        // handle a successful response
+        success: function (json) {
+            $('#post-text').val(''); // remove the value from the input
+            console.log(json); // log the returned json to the console
+            console.log("success"); // another sanity check
+        },
+
+        // handle a non-successful response
+        error: function (xhr, errmsg, err) {
+            $('#results').html("<div class='alert-box alert radius' data-alert>Oops! We have encountered an error: " + errmsg +
+                " <a href='#' class='close'>&times;</a></div>"); // add the error to the dom
+            console.log(xhr.status + ": " + xhr.responseText); // provide a bit more info about the error to the console
+        }
+    });
+};
+

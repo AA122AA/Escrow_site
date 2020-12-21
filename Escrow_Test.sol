eb0d021e72acb1b062f4f09534ccdf5fb7635895
Pavel Ogoreltsev <67636488+IamAdite@users.noreply.github.com>
2020-12-21 14:41:19 +0500

    Update Escrow_Test.sol

--- Escrow_Test.sol
+++ Escrow_Test.sol
@@ -15,7 +15,7 @@ contract EscrowBaseContract {
         uint256 OrderTime;
     }
     
-    address payable constant seller = //ПРОДАВЕЦ КОШЕЛЕК; 
+    address payable constant seller = 0x0667FA2A9dDF39d6921373FFA82E4a48C31b2a97; 
     uint256 OrderCount;
     mapping(address => uint[]) buyer_orders;
     mapping(uint => Order) order_list;

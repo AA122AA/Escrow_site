7e853cd264238df32522275b5e67ec2858b03c1e
Artem Gromov <66008477+AA122AA@users.noreply.github.com>
2020-12-21 21:14:41 +0300

    change seller address

--- Escrow_Test.sol
+++ Escrow_Test.sol
@@ -15,7 +15,7 @@ contract EscrowBaseContract {
         uint256 OrderTime;
     }
     
-    address payable constant seller = 0x7662aE8Cd04DB7B568acA1a364b43Add9d3294b7; 
+    address payable constant seller = 0xd9eaa853bbcccf5cb0a49241a7f69d743f3cf049; 
     uint256 OrderCount;
     mapping(address => uint[]) buyer_orders;
     mapping(uint => Order) order_list;
